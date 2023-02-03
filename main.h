2aa2d230a11ba78e4aa8bd3bcee17560bdb2818b
Clémentine AMEVOR <clementineamevor97@gmail.com>
2023-02-03 15:30:09 -0800

    0x11

--- main.h
+++ main.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+/**
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ */
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
 
 int _printf(const char *format, ...);
 int print_buf(char *buf, unsigned int nbuf);
